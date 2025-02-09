58c085fc5cf77abf48322a333bec87693c199b1a
Muffasa01 <nelushirendani01@gmail.com>
2025-02-09 15:25:08 +0200

    working with user controllers

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,137 @@
+import usermodel from "../models/user.model"
+import { Request, Response } from "express"
+
+const createUser = async (req: Request, res: Response) => {
+    const {username, picture, bio} = req.body
+    if(!username || !picture || !bio) {
+        res.status(400).json({
+            success: false,
+            message: "fill all the fields"
+        })
+    }
+
+    const user = await usermodel.findOne({username: username})
+
+    if(user){
+        res.status(400).json({
+            success: false,
+            message: "user exists"
+        })
+    }
+
+    const newUser  = new usermodel({
+        username,
+        picture,
+        bio
+    })
+
+    await newUser.save()
+    
+    res.status(200).json({
+        success: true,
+        message: "new user added",
+        user_details: {
+            newUser
+        }
+    })
+}
+
+const editUser = async (req: Request, res: Response) => {
+    const {username, picture, bio} = req.body
+    if(!username || !picture || !bio) {
+        res.status(400).json({
+            success: false,
+            message: "fill all the fields"
+        })
+    }
+
+    const user = await usermodel.findOneAndUpdate({username: username})
+
+    if(!user){
+        res.status(400).json({
+            success: false,
+            message: "user does not  exists"
+        })
+    }
+
+    const newUser  = new usermodel({
+        username,
+        picture,
+        bio
+    })
+
+    await newUser.save()
+
+    res.status(200).json({
+        success: true,
+        message: "user updated",
+        user_details: {
+            newUser
+        }
+    })
+}
+
+const deleteUser = async (req: Request, res: Response) => {
+    const {id} = req.params
+    if(!id){
+        res.status(400).json({
+            success: false,
+            message: "user id does not  exists"
+        })
+    }
+
+    const user = await usermodel.findOneAndDelete({_id: id})
+    if(!user){
+        res.status(400).json({
+            success: false,
+            message: "user does not  exists"
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "user deleted",
+    })
+    
+}
+
+const listUser = async (req: Request, res: Response) => {
+    const {id} = req.params
+    if(!id){
+        res.status(400).json({
+            success: false,
+            message: "user id does not  exists"
+        })
+    }
+
+    const user = await usermodel.findOne({_id: id})
+    if(!user){
+        res.status(400).json({
+            success: false,
+            message: "user does not  exists"
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+}
+
+const listUsers = async (req: Request, res: Response) => {
+    const users = await usermodel.find({})
+
+    if(!users){
+        res.status(400).json({
+            success: false,
+            message: "no users available"
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        users: {
+            users
+        }
+    })
+}
